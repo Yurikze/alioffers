bee3631f847fcc15e22bf0a4873784543087463d
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-06-27 10:13:59 +0300

    feat: add static layout

---
+++ build/index.js
@@ -0,0 +1,1 @@
+!function(){"use strict";var n=[2,3,5].map((function(n){return 2*n}));console.log(n)}();
--- header.php
+++ header.php
@@ -2,8 +2,15 @@
 <html lang="en">
 
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <?php wp_head(); ?>
 </head>
 
 <body>
-  <h2>header</h2>
+  <header class="header">
+    <div class="container">
+      <img class="header__logo" src="<?php echo get_theme_file_uri('./images/alioffers_logo.svg') ?>" alt="" />
+    </div>
+  </header>
