66e9f8fa4ac62b60470b750a536365baaf176a7e
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-06-28 09:47:10 +0300

    fix: some styling

--- front-page.php
+++ front-page.php
@@ -1,22 +1,45 @@
 <?php get_header(); ?>
 
 <div class="main container">
-      <h1 class="main__heading">Промокоды и купоны Алиэкспресс</h1>
-      <p class="main__subheading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eaque qui sit, tempore corrupti impedit accusantium optio deserunt quia tenetur.</p>
-      <section class="offers">
-          <article class="offers__item offer">
-            <h4 class="offer__heading">Промокоды от топ продавцов</h4>
-            <p class="offer__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet error a in officiis, consequatur nemo illum quidem sint! Eos, dolores.</p>
-            <ul class="offer__list">
-              <li class="offer__item">
-                <p class="offer__item-text">Купоны продавцов</p>
-                <a href="#" class="offer__item-link">Показать!</a>
-              </li>
-            </ul>
-          </article>
-      </section>
-    </main>
+  <h1 class="main__heading">Промокоды и купоны Алиэкспресс</h1>
+  <p class="main__subheading">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eaque qui sit, tempore corrupti impedit accusantium optio deserunt quia tenetur.</p>
+  <section class="offers">
 
-<?php get_footer();
+    <?php
+    $homepageOffers = new WP_Query(array(
+      'posts_per_page' => 10,
+      'post_type' => 'deal'
+    ));
 
-?>
+    while ($homepageOffers->have_posts()) {
+      $homepageOffers->the_post(); ?>
+      <article class="offers__item offer">
+        <h4 class="offer__heading"><?php the_title(); ?></h4>
+        <p class="offer__description"><?php echo get_the_content(); ?></p>
+        <ul class="offer__list">
+          <li class="offer__item">
+            <p class="offer__item-text">Купоны продавцов</p>
+            <a href="#" class="offer__item-link">Показать!</a>
+          </li>
+        </ul>
+      </article>
+    <?php }
+
+    ?>
+
+    <article class="offers__item offer">
+      <h4 class="offer__heading">Промокоды от топ продавцов</h4>
+      <p class="offer__description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet error a in officiis, consequatur nemo illum quidem sint! Eos, dolores.</p>
+      <ul class="offer__list">
+        <li class="offer__item">
+          <p class="offer__item-text">Купоны продавцов</p>
+          <a href="#" class="offer__item-link">Показать!</a>
+        </li>
+      </ul>
+    </article>
+  </section>
+  </main>
+
+  <?php get_footer();
+
+  ?>
--- functions.php
+++ functions.php
@@ -7,3 +7,25 @@ function alioffers_files()
 }
 
 add_action('wp_enqueue_scripts', 'alioffers_files');
+
+function alioffers_post_types()
+{
+  register_post_type('deal', array(
+    'supports' => array('title', 'editor'),
+    'rewrite' => array(
+      'slug' => 'deals'
+    ),
+    'public' => true,
+    'show_in_rest' => true,
+    'labels' => array(
+      'name' => 'Deals',
+      'add_new_item' => 'Add New Deal',
+      'edit_item' => 'Edit Deal',
+      'singular_name' => 'Deal'
+    ),
+    'menu_icon' => 'dashicons-plus',
+    'menu_position' => 5
+  ));
+}
+
+add_action('init', 'alioffers_post_types');
