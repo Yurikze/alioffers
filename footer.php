d2955da935231f68fff023fae06ab38782c1a5e4
Yuri Zhulev <yuri.zhulyov@gmail.com>
2024-04-25 11:05:59 +0300

    Add legal

--- footer.php
+++ footer.php
@@ -1,6 +1,12 @@
 <footer class="footer">
   <div class="container">
     <p class="footer__copyright">&#169; AliOffers.ru <?php echo date("Y"); ?></p>
+    <ul>
+      <li>Алибаба.КОМ (РУ), ООО</li>
+      <li>ИНН 7703380158 / ОГРН 1157746446747</li>
+      <li>123112, г. Москва, Пресненская наб., д. 10, ЭТ 11 ПОМ III КОМ 17</li>
+      <li>+7 (988) 016-62-81</li>
+    </ul>
   </div>
 </footer>
 </div>
